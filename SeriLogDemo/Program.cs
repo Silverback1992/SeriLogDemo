a65c869c15517b3b3c625119a8dcdabc4e865f99
Silverback1992 <44880539+Silverback1992@users.noreply.github.com>
2021-06-16 02:21:58 +0200

    Initial commit

---
+++ SeriLogDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SeriLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Serilog Simple Console Logging
+
+            SeriLogSimpleConsoleLogging.LogToConsoleMostBasic();
+            SeriLogSimpleConsoleLogging.LogToConsoleUsingGlobal();
+
+            #endregion Serilog Simple Console Logging
+        }
+    }
+}
---
+++ SeriLogDemo/SeriLogSimpleConsoleLogging.cs
@@ -0,0 +1,36 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriLogDemo
+{
+    public static class SeriLogSimpleConsoleLogging
+    {
+        public static void LogToConsoleMostBasic()
+        {
+            //The root Logger is created using LoggerConfiguration.
+
+            //This is typically done once at application start-up, and the logger saved for later use by application classes.
+            //Multiple loggers can be created and used independently if required.
+
+            using var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            log.Information("Hello World");
+        }
+
+        public static void LogToConsoleUsingGlobal()
+        {
+            //Serilog's global, statically accessible logger, is set via Log.Logger and can be invoked using the static methods on the Log class.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Information("I used the global logger!");
+        }
+    }
+}
